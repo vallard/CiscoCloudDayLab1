e63a7dfe6de8d167aadb7e958817e46dd96d24b2
Vallard Benincosa <vallard@benincosa.com>
2015-11-10 12:28:59 +0100

    updates for munich cloudweek

--- 00-Setup/Ansible/inventory/openstack_inventory.py
+++ 00-Setup/Ansible/inventory/openstack_inventory.py
@@ -45,7 +45,7 @@ OS_METADATA_KEY = {
 	'host_vars': 'ansible_host_vars'
 }
 
-OS_NETWORK_NAME = 'trial4-4007'
+OS_NETWORK_NAME = 'trial4-4003'
 
 def main(args):
 	credentials = getOsCredentialsFromEnvironment()
